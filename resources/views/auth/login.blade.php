4d67554d6d7db3bdd9ec7731aaff3d17257ac112
ABDUL MANAP <11210195@bsi.ac.id>
2025-05-29 15:29:14 +0700

    rilis

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -31,7 +31,7 @@
 
     <div class="row justify-content-center">
         <div class="col-md-6">
-         <i class="fa-regular fa-address-card"></i>
+        
           <br/>
             <div class="card">
                 <div class="card-header">{{ __('Login') }}</div>
